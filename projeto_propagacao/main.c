93ef778e6d2656e079f76b77de2d4735028a702c
GabriellaNascimento <gabriella.silva@dcomp.ufs.br>
2024-10-14 15:21:51 -0300

    ..

---
+++ projeto_propagacao/main.c
@@ -0,0 +1,28 @@
+// Numeros padronizados
+#include <stdint.h>
+// Biblioteca padrao
+#include <stdlib.h>
+// Entrada/saida padrao
+#include <stdio.h>
+
+// Funcao principal
+int main(int argc, char* argv[]) {
+	// Exibindo a quantidade de argumentos
+	printf("Quantidade de argumentos (argc): %i\n", argc);
+	// Iterando sobre o(s) argumento(s) do programa
+	for(uint32_t i = 0; i < argc; i++) {
+		// Mostrando o argumento i
+		printf("Argumento %i (argv[%i]): %s\n", i, i, argv[i]);
+	}
+	// Abrindo os arquivos com as permissoes corretas
+	FILE* input = fopen(argv[1], "r");
+	FILE* output = fopen(argv[2], "w");
+	// .
+	// .
+	// .
+	// Fechando os arquivos
+	fclose(input);
+	fclose(output);
+	// Finalizando programa
+	return 0;
+}
